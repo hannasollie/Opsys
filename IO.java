af3b81ef17402d6d97ecf22aecc4a86ba9174620
Leif Marius <lmreppen@stud.ntnu.no>
2016-04-19 16:02:31 +0000

    Added files via upload

--- IO.java
+++ IO.java
@@ -1,25 +1,83 @@
 /**
- * Created by Hanna on 18/04/2016.
+ * Created by lmari on 14.04.2016.
  */
-public class IO {
+public class IO implements  Constants{
 
+    private Queue ioQueue;
     private Statistics statistics;
     private Gui gui;
-    private Queue ioQueue;
-    private Process process;
-    private long avgIOTime;
+    private Process activeProcess;
+    private long avgIoTime;
 
 
-    public IO(Gui gui, Queue ioQueue, long avgIOTime, long queueTime, Statistics statistics) {
+    public IO(Queue ioQueue, long avgIoTime, Statistics statistics, Gui gui){
         this.gui = gui;
-        this.ioQueue = ioQueue;
-        this.avgIOTime = avgIOTime;
         this.statistics = statistics;
+        this.ioQueue = ioQueue;
+        this.avgIoTime = avgIoTime;
+
     }
 
-    private Process popIoQueue(){
+
+
+    public  Event addIorequest(Process activeProcess, long clock){
+        //add process to I/0 queue
+
+        ioQueue.insert(activeProcess);
+        //activeProcess.calculateTimeToNextIoOperation();
+        if(noActiveProcess()){
+            return startIoOperation(clock);
+        }
+
+        return  null;
+    }
+
+    public Event startIoOperation(long clock){
+    //check if a new I/O operations
+        if(activeProcess == null){
+            //device is free
+        }
+        if(!ioQueue.isEmpty()){
+
+            activeProcess = popNextProcess();
+            activeProcess.enteredIo(clock);
+            gui.setIoActive(activeProcess);
+            statistics.nofCompletedProcesses++;
+
+            //start first  process in the queue start I/O
+
+
+        }
+        return new Event(END_IO, clock + avgIoTime);
+    }
+    public Process endIoOperation(long clock){
+        this.ioQueue = null;
+        Process process = this.activeProcess;
+        process.leftIo(clock);
+        gui.setIoActive(this.activeProcess);
+        return process;
+    }
+
+
+    public boolean noActiveProcess(){
+        return this.activeProcess instanceof Process;
+    }
+
+    public Process popNextProcess(){
         return (Process) ioQueue.removeNext();
     }
 
+    public long getIoTime(){
+        return (long) (Math.random() * avgIoTime *2);
+
+    }
+
+
+    public void calculateTimePassed(long timePassed) {
+        statistics.ioQueueLengthTime += ioQueue.getQueueLength()*timePassed;
+        if (ioQueue.getQueueLength() > statistics.ioQueueLargestLength)
+            statistics.ioQueueLargestLength= ioQueue.getQueueLength();
+    }
 
 }
+
